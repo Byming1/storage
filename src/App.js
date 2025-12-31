b4a00aa20a02866d437f67defc8987d95712e0f6
Byming1 <pqm1290@gmail.com>
2025-12-31 20:36:00 +0700

    'test database'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
-console.log(process.env.REACT_APP_SUPABASE_URL)
+import { supabase } from './supabaseClient'
+
+const testUpdate = async () => {
+  const { error } = await supabase
+    .from('movies')
+    .update({ title: 'UPDATE FROM LOCAL' })
+    .eq('id', 1)
+
+  console.log(error)
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the Store</h1>
-
-
+      <button onClick={testUpdate}>Test Update</button>
     </div>
   );
 }
